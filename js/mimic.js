158013dc8590ae10aeb4ce9aceea75e15b2cda42
Andrew Keym <andrew.keym@gmail.com>
2013-06-03 20:31:53 -0400

    Added simple remove method. Cleaned up some code in replicate method.

--- js/mimic.js
+++ js/mimic.js
@@ -14,16 +14,16 @@
         this.indicies = [];
         this.last_index = 0;
         if ($(this.options.mimicEl).length === 1) {
-          $(this.options.mimicEl).attr('id').replace(/\d/, function(str) { 
-            obj.last_index = parseInt(str) + 1; 
-          });
+            $(this.options.mimicEl).attr('id').replace(/\d/, function(str) { 
+                obj.last_index = parseInt(str) + 1; 
+            });
         }
         if (this.options.limit) {
             for (var i=0; i<this.options.limit; i++) { 
-              this.indicies.push(i);
-              if ( i === this.options.limit) {
-                 this.last_index = i; 
-              }
+                this.indicies.push(i);
+                if ( i === this.options.limit) {
+                    this.last_index = i; 
+                }
             } 
         }
 
@@ -44,17 +44,34 @@
                 $clone.attr('id', $clone.attr('id').replace(/\d/, index));
                 // update children's ID and names
                 $clone.find('[id]').each(function() { 
-                  $(this).attr('id', $(this).attr('id').replace(/\d/, index) );
-                }).find('[name]').each(function() { 
-                  $(this).attr('name', $(this).attr('name').replace(/\d/, index) );
+                    $(this).attr('id', $(this).attr('id').replace(/\d/, index) );
                 });
-
+                $clone.find('[name]').each(function() { 
+                    $(this).attr('name', $(this).attr('name').replace(/\d/, index) );
+                });
+              
+                // adding the remove link
+                var $removelink_container = $clone.find('[data-mimic-remove]');
+                $removelink_container.append('<a href="#">Remove</a>');
+                var $removelink = $removelink_container.children('a');
+                $removelink.attr({
+                    'data-mimic-remove': '#' + $clone.attr('id'),
+                    'data-mimic-src': this.element.data('mimicEl')
+                });              
+              
                 // insert the clone before trigger
                 $clone.insertBefore(this.element);
+              
+                // update indicies
+                this.indicies.push(index);
 
             } else {
                 console.log("Mimicked element doesn't exist or is referring to too many elements: " + this.options.mimicEl);
             }
+        },
+      
+        remove: function () {
+            $(this.options.mimicRemove).remove();
         }
     };
 
@@ -65,14 +82,13 @@
     $.fn.mimic = function (option) {
         return this.each(function () {
             var $this = $(this),
+                $el = $el ? $el : "",
                 data = $this.data('mimic');
             if (!data) {
                 $this.data('mimic', (data = new Mimic(this)));
             }
             if (typeof option === 'string') {
-                if (option) {
-                    data[option](); 
-               } 
+               data[option]();
             } else {
                data['replicate']();
             }
@@ -90,6 +106,11 @@
         $('body').on("click", "[data-mimic-el]", function (e) {
             e.preventDefault();
             $(this).mimic();
+        });
+      
+        $('body').on("click", "[data-mimic-remove] a", function (e) {
+            e.preventDefault();
+            $(this).mimic('remove'); 
         });
     });
 }(window.jQuery));
